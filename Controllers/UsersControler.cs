72e137fff1ee892a0a5b40492f6f3e60ccf53736
Diogo Petroski <diogopetroski@gmail.com>
2026-07-01 16:28:05 -0300

    criação do projeto

---
+++ Controllers/UsersControler.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("Projeto funcionando!");
+    }
+}
